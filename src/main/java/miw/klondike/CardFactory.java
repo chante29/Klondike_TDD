dbee98070e7733bbc17836e0a07afdb5e24d6a71
Chante <Chante@192.168.1.130>
2015-04-15 23:40:26 +0200

    Se refactoriza CardFactory

--- src/main/java/miw/klondike/CardFactory.java
+++ src/main/java/miw/klondike/CardFactory.java
@@ -20,176 +20,33 @@ public class CardFactory {
 	public static Card getCard(Score score, Suit suit, boolean covered) {
 		switch(score){
 		case AS:
-			switch(suit){
-			case HEARTS:
-				return new As(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new As(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new As(Suit.CLUBS, covered);
-			case SPADES:
-				return new As(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new As(suit, covered);
 		case TWO:
-			switch(suit){
-			case HEARTS:
-				return new Two(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Two(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Two(Suit.CLUBS, covered);
-			case SPADES:
-				return new Two(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Two(suit, covered);
 		case THREE:
-			switch(suit){
-			case HEARTS:
-				return new Three(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Three(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Three(Suit.CLUBS, covered);
-			case SPADES:
-				return new Three(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Three(suit, covered);
 		case FOUR:
-			switch(suit){
-			case HEARTS:
-				return new Four(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Four(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Four(Suit.CLUBS, covered);
-			case SPADES:
-				return new Four(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Four(suit, covered);
 		case FIVE:
-			switch(suit){
-			case HEARTS:
-				return new Five(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Five(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Five(Suit.CLUBS, covered);
-			case SPADES:
-				return new Five(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Five(suit, covered);
 		case SIX:
-			switch(suit){
-			case HEARTS:
-				return new Six(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Six(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Six(Suit.CLUBS, covered);
-			case SPADES:
-				return new Six(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Six(suit, covered);
 		case SEVEN:
-			switch(suit){
-			case HEARTS:
-				return new Seven(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Seven(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Seven(Suit.CLUBS, covered);
-			case SPADES:
-				return new Seven(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Seven(suit, covered);
 		case EIGHT:
-			switch(suit){
-			case HEARTS:
-				return new Eight(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Eight(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Eight(Suit.CLUBS, covered);
-			case SPADES:
-				return new Eight(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Eight(suit, covered);
 		case NINE:
-			switch(suit){
-			case HEARTS:
-				return new Nine(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Nine(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Nine(Suit.CLUBS, covered);
-			case SPADES:
-				return new Nine(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Nine(suit, covered);
 		case TEN:
-			switch(suit){
-			case HEARTS:
-				return new Ten(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Ten(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Ten(Suit.CLUBS, covered);
-			case SPADES:
-				return new Ten(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Ten(suit, covered);
 		case VALET:
-			switch(suit){
-			case HEARTS:
-				return new Valet(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Valet(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Valet(Suit.CLUBS, covered);
-			case SPADES:
-				return new Valet(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Valet(suit, covered);
 		case DAME:
-			switch(suit){
-			case HEARTS:
-				return new Dame(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Dame(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Dame(Suit.CLUBS, covered);
-			case SPADES:
-				return new Dame(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
+			return new Dame(suit, covered);
 		case ROI:
-			switch(suit){
-			case HEARTS:
-				return new Roi(Suit.HEARTS, covered);
-			case DIAMONDS:
-				return new Roi(Suit.DIAMONDS, covered);
-			case CLUBS:
-				return new Roi(Suit.CLUBS, covered);
-			case SPADES:
-				return new Roi(Suit.SPADES, covered);
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
-			}
-				default:
-					throw new IllegalArgumentException("Unexpected type card");
+			return new Roi(suit, covered);
+		default:
+			throw new IllegalArgumentException("Unexpected type card");
 			
 		}
 	}
