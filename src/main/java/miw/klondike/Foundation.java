b6c94c4e8d455e352792ec04be5b3fbb86a1d7d4
Chante <Chante@192.168.1.130>
2015-04-17 19:27:10 +0200

    Se pone de nuevo el Score con puntuación para recuperar la puntuación de la carta a través del número y quitar un switch

--- src/main/java/miw/klondike/Foundation.java
+++ src/main/java/miw/klondike/Foundation.java
@@ -12,48 +12,7 @@ public class Foundation extends StackCards{
 		super();
 		assertFalse(numCards > 12);
 		for (int numCard = 1; numCard <= numCards; numCard++) {
-			switch(numCard){
-			case 1:
-				this.addCard(CardFactory.getCard(Score.AS, suit, false));
-				break;
-			case 2:
-				this.addCard(CardFactory.getCard(Score.TWO, suit, false));
-				break;
-			case 3:
-				this.addCard(CardFactory.getCard(Score.THREE, suit, false));
-				break;
-			case 4:
-				this.addCard(CardFactory.getCard(Score.FOUR, suit, false));
-				break;
-			case 5:
-				this.addCard(CardFactory.getCard(Score.FIVE, suit, false));
-				break;
-			case 6:
-				this.addCard(CardFactory.getCard(Score.SIX, suit, false));
-				break;
-			case 7:
-				this.addCard(CardFactory.getCard(Score.SEVEN, suit, false));
-				break;
-			case 8:
-				this.addCard(CardFactory.getCard(Score.EIGHT, suit, false));
-				break;
-			case 9:
-				this.addCard(CardFactory.getCard(Score.NINE, suit, false));
-				break;
-			case 10:
-				this.addCard(CardFactory.getCard(Score.TEN, suit, false));
-				break;
-			case 11:
-				this.addCard(CardFactory.getCard(Score.VALET, suit, false));
-				break;
-			case 12:
-				this.addCard(CardFactory.getCard(Score.DAME, suit, false));
-				break;
-			case 13:
-				this.addCard(CardFactory.getCard(Score.ROI, suit, false));
-				break;
-			}
-			
+			this.addCard(CardFactory.getCard(Score.valueOf(numCard), suit, false));			
 		}
 		this.suitCard = SuitCardFactory.getSuitCard(suit);
 	}
